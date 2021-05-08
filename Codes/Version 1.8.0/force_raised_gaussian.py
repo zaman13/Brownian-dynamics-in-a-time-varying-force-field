a784f543b630cebbae43c36123c27cd12aba606b
Mohammad Asif Zaman <39745895+zaman13@users.noreply.github.com>
2021-05-08 14:43:08 -0700

    Version 1.8.0

---
+++ Codes/Version 1.8.0/force_raised_gaussian.py
@@ -0,0 +1,263 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on March 6, 2021
+
+@author: Mohammad Asif Zaman
+
+
+- April 10, 2021
+        - Added optical spot ON-OFF text
+- April 11, 2021
+        - Changed axis limit units to microns
+        
+"""
+
+import numpy as np
+import pylab as py
+import matplotlib as plt
+from parameters import *
+
+# Module global Parameters:
+# =============================================================================
+# Force parameters
+r_active = 0
+n_order = 1             # Order of the Gaussian potential = 2n
+w_well = 10e-6          # 1/e *max width of the potential well
+A_well = 500*k_B*T      # well depth
+rbase = 1e-6
+
+
+
+# Setting fluid flow velocity
+vx_flow = 0
+vy_flow = 0
+vz_flow = 0
+
+# p_wall_set = [[0,0,1e-6],[0,1e-6,0]]
+# d_wall_set = [5e-6,2e-6]
+
+p_wall_set = [[0,0,1]]                  # plane coefficient vector array
+d_wall_set = [0]                        # plane right-hand term array
+
+# Limits on the extent of the walls. Format: [xmin, xmax, ymin, ymax, zmin, zmax] per wall
+lim_wall_set = [[-np.inf, np.inf, -np.inf, np.inf, -np.inf, np.inf],     
+               ]
+
+
+p_wall_set = np.array(p_wall_set)
+d_wall_set = np.array(d_wall_set)
+lim_wall_set = np.array(lim_wall_set)
+
+
+# Particle parameters (number and raidus array)
+def time_pos_ax_limits():
+    Np = 4                             # Number of particles
+    # ro =  np.zeros((Np,1)) + 2e-6
+    ro =  np.zeros(Np) + 2e-6
+    ro[0] = 1.5e-6
+    ro[1] = 2.5e-6
+    
+    # Time parameter
+    tfinal = 12  #12
+    
+    
+    # Axes parameter (in microns)
+    
+    x_lim = [-30, 30]
+    y_lim = [-30, 30]
+    z_lim = [-5,  25]
+    
+    
+    # Limit of initial particle positions (in meters)
+    xi_lim = [-10e-6, 10e-6]
+    yi_lim = [-10e-6, 10e-6]
+    zi_lim = [max(ro)*1.5, 20e-6]
+    
+    return Np, ro, tfinal, x_lim, y_lim, z_lim, xi_lim, yi_lim, zi_lim
+
+
+
+
+
+# =============================================================================
+
+
+def particle_color(p):
+    cl_p = cl_dred
+ 
+    return cl_p
+
+
+
+
+def draw_static_geo(ax_xy, ax_yz, ax_xz):
+     Np, ro, tfinal, x_lim, y_lim, z_lim, xi_lim, yi_lim, zi_lim = time_pos_ax_limits()
+     py.gcf().sca(ax_yz)
+     substrate_yz = py.Rectangle((y_lim[0], z_lim[0]),(y_lim[1] - y_lim[0]), abs(z_lim[0]),fc='#d4d4d4', ec='k')
+     py.gca().add_patch(substrate_yz)
+    
+     py.gcf().sca(ax_xz)
+     substrate_xz = py.Rectangle((x_lim[0], z_lim[0]),(x_lim[1] - x_lim[0]), abs(z_lim[0]),fc='#d4d4d4', ec='k')
+     py.gca().add_patch(substrate_xz)
+    
+     py.gcf().sca(ax_xy)
+     substrate_xy = py.Rectangle((x_lim[0], y_lim[0]), (x_lim[1] - x_lim[0]), (y_lim[1] - y_lim[0]),fc='#f9f9f9')
+     py.gca().add_patch(substrate_xy)
+        
+     return 0
+ 
+    
+
+def draw_dynamic_geo(ax_xy, ax_yz, ax_xz):
+    patch_spot_xy = py.Circle((0, 0), 0.5*w_well*1e6, fc='#ff8c00',alpha = 0.8)
+    # patch_spot_yz = plt.patches.Arc((0, 0), 0.5*w_well*1e6, 0.5*w_well*1e6,0, 0, 180, fc='#ff8c00',alpha = 0.8)
+    
+    
+    py.gcf().sca(ax_xy)
+    py.gca().add_patch(patch_spot_xy)
+    
+    # py.sca(ax_yz)
+    # py.gca().add_patch(patch_spot_yz)
+    
+    
+    return 0
+
+
+    
+    
+    
+def draw_geo(tm, ax_xy, ax_yz, ax_xz):
+    # March 7, 2021
+    
+    # The flag_source_state variable is used to draw/erase the source geometry only once
+    # This is necessary to speed up the animation.
+    global flag_source_state_1  # Make this variable global so that the assigned value remains saved globally as t changes
+    global flag_source_state_2
+    global str1
+    global str2
+ 
+    if 'flag_source_state_1' not in globals():
+        global flag_source_state     # Make this variable global so that the assigned value remains saved globally as t changes
+        flag_source_state_1 = 0        # initialize with OFF state
+        print('Defining global flag for source geometry \n')
+    
+    if 'flag_source_state_2' not in globals():
+        global flag_source_state       # Make this variable global so that the assigned value remains saved globally as t changes
+        flag_source_state_2 = 0        # initialize with OFF state
+        print('Defining global flag for source geometry \n')
+      
+    
+    # Draw static geometry (only once)
+    if flag_source_state_2 < 1:
+        draw_static_geo(ax_xy, ax_yz, ax_xz)
+        flag_source_state_2 = 1 
+        
+    
+    # Draw source
+    if (tm > 1) & (tm < 8) & (flag_source_state_1 < 1):
+        draw_dynamic_geo(ax_xy, ax_yz, ax_xz)
+        str1 = 'Optical beam ON'
+        str2 = ''
+        # text_string2.set_text(str2)  
+        # ax_xy.text(0.05, 0.8, 'Optical spot ON',color = '#FF0000',transform=ax_xy.transAxes)
+        flag_source_state_1 = 1
+        print('Drawing source\n')
+        
+        
+        
+        
+    # Erase source (draw a white circle)   
+    if (tm > 8) & (flag_source_state_1 == 1): 
+        patch_spot = py.Circle((0, 0), 0.51*w_well*1e6, fc='#f9f9f9',alpha = 1)
+        py.gca().add_patch(patch_spot)
+        str1 = 'Optical beam OFF'
+        str2 = ''
+        # ax_xy.text(0.05, 0.8, 'Optical spot ON',color = '#f9f9f9', transform=ax_xy.transAxes)
+        # ax_xy.text(0.05, 0.8, 'Optical spot OFF',color = '#FF0000',transform=ax_xy.transAxes)
+        print('Erasing source\n')
+        flag_source_state_1 = 0
+    
+    if 'str1' not in globals():
+        str1 = ''
+        str2 = ''
+    
+    return str1, str2
+   
+    
+
+# def draw_yz(tm):
+#     substrate_yz = py.Rectangle((-xrange_limit*1e6, zlow_limit*1e6),2*xrange_limit*1e6, abs(zlow_limit)*1e6,fc='#d4d4d4', ec='k')
+#     py.gca().add_patch(substrate_yz)
+    
+
+# def draw_xz(tm):
+#     substrate_xz = py.Rectangle((-xrange_limit*1e6, zlow_limit*1e6),2*xrange_limit*1e6, abs(zlow_limit)*1e6,fc='#d4d4d4', ec='k')
+#     py.gca().add_patch(substrate_xz)
+
+
+# This is function that is called from the main program
+# Simplified spring force model
+def force_profile(r_in, t):
+    
+    # Np = r_in[0,:].size
+    Np, ro, tfinal, x_lim, y_lim, z_lim, xi_lim, yi_lim, zi_lim = time_pos_ax_limits()
+    
+    
+    
+    fm = np.zeros((3,Np))
+    
+    
+
+    r_norm = np.linalg.norm(r_in, axis = 0) + 1e-30
+    
+    g = A_well*np.exp(-(r_norm/w_well)**(2*n_order))/rbase**3
+    
+    
+        
+    if (t > 1) & (t<8):
+        fm[0,:] = -2*n_order*r_in[0,:]/(r_norm**2) * (r_norm/w_well)**(2*n_order) * g *(ro)**3
+        fm[1,:] = -2*n_order*r_in[1,:]/(r_norm**2) * (r_norm/w_well)**(2*n_order) * g *(ro)**3
+        fm[2,:] = -2*n_order*r_in[2,:]/(r_norm**2) * (r_norm/w_well)**(2*n_order) * g *(ro)**3
+        # fm[0,:] = np.gradient(g,r_in[0,:]) 
+        # fm[1,:] = np.gradient(g,r_in[1,:]) 
+        # fm[2,:] = np.gradient(g,r_in[2,:]) 
+    
+
+    
+    # fm[:,2] = 0
+    # fm[:,3] = 0
+    # fm[:,4] = 0
+    # fm[:,5] = 0
+    # fm[:,6] = 0
+     
+    
+    return fm
+
+
+def force_plot():
+    Np, ro, tfinal, x_lim, y_lim, z_lim, xi_lim, yi_lim, zi_lim = time_pos_ax_limits()
+    
+    
+    
+    r_in = np.linspace(x_lim[0]*.7e-6,x_lim[1]*.7e-6,200)
+    F = np.zeros(200)
+    
+    for m in range(r_in.size):
+        temp = r_in[m]
+        temp2 = np.tile(temp, (3,Np))
+        temp3 = force_profile(temp2, 2)
+        F[m] = temp3[0,1]
+    
+    py.figure()
+    py.plot(r_in*1e6,F*1e12/(ro[1]*1e6)**3)
+    # py.plot(r_in[1,:]*1e6,F[1,:]*1e12,'.', label = '$F_y$')
+    # py.plot(r_in[2,:]*1e6,F[2,:]*1e12,'x', label = '$F_z$')
+    py.xlabel('Distance, $r$ ($\mu$m)')
+    py.ylabel('$F_r/r_o^3$ ,  (pN/$\mu m^3$)')
+    # py.legend()
+
+# force_plot()
+
+
+
---
+++ Codes/Version 1.8.0/parameters.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 19 16:47:35 2021
+
+@author: Mohammad Asif Zaman
+
+- May 6, 2021
+    - Added color library that can be accessed in other modules
+
+"""
+
+
+# Parameters (All SI units)
+# =============================================================================
+k_B = 1.38e-23             # Boltzmann constant
+T = 300                    # Temperature (K)
+eta = 8.9e-4               # Dynamic viscosity of water (PaS) 
+
+
+frame_rate = 30                 # Animation frame rate in fps
+Nt =frame_rate*40 # 300 #1501   # Number of time steps
+
+
+rho = 1055                      # density of polystyrene beads in kg/m3
+damping_factor = 0              # Fraction of the velocity that is lost during each collision
+
+
+# Np = number of particles, defined inside the force_() functions
+# ro = (Np,1) raidus vector, defined inside the force_() functions
+# tfinal = final simulation time, defined inside the force_() functions
+
+
+# Colors
+# =============================================================================
+cl_gold = '#d4af37'            # Golded yellow
+cl_army_green = '#5a6b34'      # Dark green
+cl_lgrey = '#f9f9f9'           # light grey
+cl_dgrey = '#d4d4d4'           # Dark grey
+cl_yellow = '#ff8c00'          # yellow
+cl_royal_blue = '#002366'      # royal blue
+cl_dred = '#C21808'            # dark red
+cl_ored = '#fd3b01'            # orange red
+cl_bblue = '#add8e6'           # baby blue
+cl_pblue = '#78a2cc'           # pastel blue
